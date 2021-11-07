fafdd2119e7bef9ae72efd8af5685c7be02c440a
macbook <macbook@chrismac.local>
2021-11-07 17:16:49 +0100

    Initial Commit

---
+++ Cickmode1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cickmode1
+//
+//  Created by macbook on 11/7/21.
+//  Copyright © 2021 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
